d7b9a9659915f2a1448235f7e2b316d840f2748c
Kaeli <flanagan.kaeli@gmail.com>
2025-12-08 14:47:40 -0800

    set up dummy values

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const partnersStack = require('./src/data/partners');
+
 const app = express();
 
 app.use(cors());
 
 app.get("/getStack", (req, res) => {
-    res.json({"users": ["userOne", "userTwo", "userThree"]});
+    res.json({
+        stack: partnersStack,
+        count: partnersStack.length,
+    });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
